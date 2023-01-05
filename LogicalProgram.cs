9439a99105933dc0adcdc7e3391e12a60233e338
Sangmesh Methre <ssmcmethre3080@gmail.com>
2023-01-06 00:43:51 +0530

    Logical Program

---
+++ LogicalProgram.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgramUC2
+{
+    public  class LogicalProgram
+    {
+        public static void PerfectNumber()
+        {
+            int number, sum = 0, n;
+            Console.Write("Enter The  Number ");
+            number = int.Parse(Console.ReadLine());
+            n = number;
+            for (int i = 1; i < number; i++)
+            {
+                if (number % i == 0)
+                {
+                    sum = sum + i;
+                }
+            }
+            if (sum == n)
+            {
+                Console.WriteLine("\n Enter The Number Is Perfect Number");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("\n Enter The Number Is Not A Perfect Number");
+                Console.ReadLine();
+            }
+        }
+    }
+}
