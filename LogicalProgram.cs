e25dbc55e09e792e0c95630122188e6dd40fcfc6
Sangmesh Methre <ssmcmethre3080@gmail.com>
2023-01-06 00:50:54 +0530

    Logical Program

---
+++ LogicalProgram.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgramUC4
+{
+    public  class LogicalProgram
+    {
+        public static void ReverseNumber()
+        {
+            int n, reverse = 0, rem;
+            Console.WriteLine("Enter A Number");
+            n= int.Parse(Console.ReadLine());
+            while(n!=0)
+            {
+                rem = n % 10;
+                reverse = reverse * 10 + rem;
+                n/=10;
+            }
+            Console.WriteLine("Reversed Number:" + reverse);
+        }
+    }
+}
